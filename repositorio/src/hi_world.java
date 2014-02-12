b275abe922e46dca28a5704163699425934613e0
antroar <antu_8889@hotmail.com>
2014-02-12 01:38:25 -0600

    creacion
    se detecta nuevos repositorios

---
+++ repositorio/src/hi_world.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AnToine
+ */
+public class hi_world {
+    public static void main (String args[]){
+        JOptionPane.showMessageDialog(null, "hola mundo");
+    }
+    
+}
